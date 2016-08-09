7be9aa2f73867ca4c54ca86d5ea9fcfe2163f1cf
Stephen <stevemuller@gmail.com>
2016-08-09 16:20:02 -0700

    testing

--- round.py
+++ round.py
@@ -1,43 +1,45 @@
 """Stores a single round"""
 
+import time
+
+
 
 class Round:
     """stores a single round of trap"""
     def __init__(self, player):
         self.singles_round = []
         self.player = player
-        self.date = date
-        self.starting_station = starting_station
-        self.shotgun_shells = shotgun_shells
-        self.gun = gun
-        self.excuses = excuses
+        self.date = time.strftime('%Y-%m-%d %H:%M:%S')
+        self.starting_station = 1
+        # self.shotgun_shells = shotgun_shells
+        # self.gun = gun
+        # self.excuses = excuses
 
     def __eq__(self, other):
         self.singles_round = other.singles_round
         self.player = other.player
         self.date = other.date
         self.starting_station = other.starting_station
-        self.shotgun_shells = other.shotgun_shells
-        self.gun = other.gun
-        self.excuses = other.excuses
+        # self.shotgun_shells = other.shotgun_shells
+        # self.gun = other.gun
+        # self.excuses = other.excuses
 
     def __repr__(self):
-        return 'Round({!r}{!r}{!r}{!r}{!r}{!r}{!r})'.format(
-            singles_round, player, date, starting_station, shotgun_shells,
-            gun, excuses
+        return 'Round({!r}{!r}{!r}{!r})'.format(
+            singles_round, player, date, starting_station
         )
 
-def hit_miss(hit_or_miss):
-    """Adds result of each shot to a list stored in a round class.
+    def hit_miss(self, hit_or_miss):
+        """Adds result of each shot to a list stored in a round class.
 
-    Expects maybe to be a boolean.
+        Expects maybe to be a boolean.
 
-    >>> a = Round('goober')
-    >>> a.hit_miss(True)
-    >>> a.hit_miss(False)
-    >>> a
-    """
-    if hit_or_miss:
-        self.singles_round.append([True])
-    else:
-        self.singles_round.append([False])
+        >>> a = Round('goober')
+        >>> a.hit_miss(True)
+        >>> a.hit_miss(False)
+        >>> a
+        """
+        if hit_or_miss:
+            self.singles_round.append([True])
+        else:
+            self.singles_round.append([False])
